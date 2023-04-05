b5497579625f43c0bc336f9b23343e445ba88e7f
MAGISKDEVELOPER <110710093+MAGISKDEVELOPER@users.noreply.github.com>
2023-04-05 16:49:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from os import environ
 
 from pyrogram import Client, idle
 
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-SESSION_NAME = environ["SESSION_NAME"]
+API_ID = int(environ["21124451"])
+API_HASH = environ["d0c75e0e8ae1f79ddad10a033411f9ed"]
+SESSION_NAME = environ["BQCJbNuNhD5e5rcC1P7h0XKFBOHfvh5Ir-TkEOrULq7ho-ngxaIdzJsuyxLAnzlWy8H41Fmt2jmcVulpDGiMh1sHNbXH3BTE1gE4fBspSk_o79nY4QT6ntdU_fBqg7mImh48vAzYu93-A_p_FPiqssjwB0FUWU8xDRkDtNr1eY1eszzlbBDp-u51zA20okv7OuQ5-rLy9OqTzyu9bTjV7bKYiiNj_QoLWt424XT7_79AwTvdqaPjltB8tHxv919bxam0lfeJnCt8P06SOuZskvtlxRTEF4JvnPANnFx9g16BGQRLkI3TdSwEXY5E4snwJDAbjHgxRDxlRtHh0GqJ4D56AAAAAWTX6CIA"]
 
 PLUGINS = dict(
     root="plugins",
